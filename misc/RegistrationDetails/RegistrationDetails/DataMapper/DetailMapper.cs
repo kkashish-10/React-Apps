8864394b69ab7aab2846c73ca2e28b8b2d527fa0
KASHISH <kd372744@gmail.com>
2023-02-17 11:08:21 +0530

    added rishu
    rishab c# code

---
+++ misc/RegistrationDetails/RegistrationDetails/DataMapper/DetailMapper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace DataMapper
+{
+    public class DetailMapper
+    {
+       
+    string ConnectionString = @"Data Source= IGL183259;Initial Catalog = master; Integrated Security = True";
+
+    public int SaveDetails(string Name, string Email, string Address, string Mobile, string Country, string State, string City, string Course)
+
+    {
+        using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SubmitDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", Name);
+                cmd.Parameters.AddWithValue("@Email", Email);
+                cmd.Parameters.AddWithValue("@Address", Address);
+                cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                cmd.Parameters.AddWithValue("@Country", Country);
+                cmd.Parameters.AddWithValue("@State", State);
+                cmd.Parameters.AddWithValue("@City", City);
+                cmd.Parameters.AddWithValue("@Course", Course);
+                int i= cmd.ExecuteNonQuery();
+                con.Close();
+                return i;
+            }
+        }
+
+        public string GetAllState(string country)
+        {
+            string s= "";
+            SqlConnection con;
+            using (con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("GetAllState", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Country", country);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    s += Convert.ToString(dr["state"])+",";
+                }
+                con.Close();
+                
+                return s;
+            }
+        }
+        
+    public string GetAllState(string state,string country)
+        {
+            string s= "";
+            SqlConnection con;
+            using (con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("GetAllCity", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@State", state);
+                cmd.Parameters.Add("@Country", country);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    s += Convert.ToString(dr["city"])+",";
+                }
+                con.Close();
+                
+                return s;
+            }
+        }
+        
+        public int checkEmail(string Email)
+        {
+            int c = 0;
+            SqlConnection con;
+            using (con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("CheckDuplicateEmail", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Email", Email);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                     c = Convert.ToInt32(dr["CountEmail"]);
+                }
+                con.Close();
+                if (c > 0)
+                {
+                    c = 1;
+                }
+                return c;
+            }
+        }
+
+         public List<string> GetAllCountry()
+        {
+            List<string> str = new List<string>();
+            SqlConnection con;
+            using (con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("GetAllCountry", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    str.Add(Convert.ToString(dr["country"]));
+                }
+                con.Close();
+
+                return str;
+            }
+        }
+    }
+}
+
---
+++ misc/RegistrationDetails/RegistrationDetails/RegistrationDetails/Controllers/RegistrationController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationDetails.Models;
+using DataMapper;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+namespace RegistrationDetails.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        List<SelectListItem> courseList = new List<SelectListItem>();
+        DetailMapper detailMapper = new DetailMapper();
+        string ConnectionString = @"Data Source= IGL183259;Initial Catalog = master; Integrated Security = True";
+        // GET: Registration
+        public ActionResult Index()
+        {
+            
+            List<SelectListItem> list = new List<SelectListItem>();
+            if (ViewBag.CountryList == null)
+            {
+                var ListOfCountry = detailMapper.GetAllCountry();
+   
+                for (int i = 0; i < ListOfCountry.Count; i++)
+                {
+                    list.Add(new SelectListItem
+                    {
+                        Text = ListOfCountry[i],
+                        Value = ListOfCountry[i]
+                    });
+                }
+            }
+            ViewBag.CountryList = list;
+            List<SelectListItem> CityList = new List<SelectListItem>();
+            ViewData["CityList"] = CityList;
+            ViewBag.StateList = courseList;
+            var obj=new RegistrationDetailsModel();
+            var ListAllDATA = new List<RegistrationDetailsModel>();
+            SqlConnection con;
+            using (con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("GetAllData", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    var temp = new RegistrationDetailsModel();
+                    temp.Name = Convert.ToString(dr["Name"]);
+                    temp.Email = Convert.ToString(dr["Email"]);
+                    temp.Address = Convert.ToString(dr["Address"]);
+                    temp.Country = Convert.ToString(dr["Country"]);
+                    temp.City = Convert.ToString(dr["City"]);
+                    temp.Course = Convert.ToString(dr["Course"]);
+                    temp.State = Convert.ToString(dr["State"]);
+                    temp.Mobile = Convert.ToString(dr["Mobile"]);
+
+                    ListAllDATA.Add(temp);
+                }
+                obj.dataList = ListAllDATA;
+                con.Close();
+            }
+            return View(obj);
+            
+        }
+        
+        public ActionResult Submit(RegistrationDetailsModel registrationDetailsModel)
+        {
+           
+            if (Request.Form["state"] != null)
+            {
+                var st = Request.Form["state"].ToString();
+            }
+            
+            int result = detailMapper.SaveDetails(registrationDetailsModel.Name, registrationDetailsModel.Email,
+                registrationDetailsModel.Address, registrationDetailsModel.Mobile, registrationDetailsModel.Country, registrationDetailsModel.State, registrationDetailsModel.City, registrationDetailsModel.Course);
+            Index();
+            
+            return View("Index");
+        }
+
+        [HttpPost]
+        public string GetAllState(string country)
+        {
+           
+            return detailMapper.GetAllState(country);
+            
+        }
+        [HttpPost]
+        public string GetAllCity(string state,string country)
+        {
+           
+            return detailMapper.GetAllState(state,country);
+            
+        }
+        [HttpPost]
+        public string checkEm(string email)
+        {
+            string result = "";
+           
+
+            int c = detailMapper.checkEmail(email);
+            if (c > 0)
+            {
+
+                result = "1";
+
+            }
+            else {
+                result = "0";
+            }
+            return result;
+        }
+          
+
+    }
+}
